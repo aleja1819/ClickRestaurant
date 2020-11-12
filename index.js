e9eb919dd88d48e2dec0746f491902e3aedac22a
rodrigo gonzalez carrasco <rodgonzalez760@gmail.com>
2020-11-12 11:56:02 -0300

    add viws al index.js

--- index.js
+++ index.js
@@ -14,24 +14,31 @@ Y agregamos en el package.json lo siguiente:
 */
 import express from 'express';
 import router from './routes/index.js';
-import db from './config/db.js';
+import db from './config/db.js'; 
+import bodyParser from 'body-parser';
+import path from 'path';
+import config from './config';
+import routes from './routes';
 
 const app = express();
+
+/**Puerto y Host para la app */
+// Definir host 
+const host = process.env.HOST || '0.0.0.0';
+// Definir puerto
+const port = process.env.PORT || 4000;
+
+
+// Habilitar Pug
+app.set('view engine', 'pug');
+
+app.set('views', path.join(_dirname, './views'));
 
 // Conectar la base de datos
 db.authenticate()
     .then( () => console.log('Base de datos conectada!'))
     .catch( error => console.log(error));
 
-/**Puerto y Host para la app */
-
-// Definir host 
-const host = process.env.HOST || '0.0.0.0';
-// Definir puerto
-const port = process.env.PORT || 4000;
-
-// Habilitar Pug
-app.set('view engine', 'pug');
 
 // Obtener el año actual
 app.use( (req, res, next) => {
